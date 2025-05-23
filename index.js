16226c06f785a00f7acc2317848ec13364b9da1a
Tyler <tydcarl@gmail.com>
2025-05-22 22:59:23 -0700

    spinner

--- index.js
+++
@@ -1,7 +0,0 @@
-const spinner = document.getElementById("spinner");
-    spinner.style.display = "block"; // Show spinner
-    // Simulating a loading time (remove this and add your logic)
-    setTimeout(() => {
-        spinner.style.display = "none"; // Hide spinner after some time
-    }, 2000); // Change 2000 to the time you need for your process
-}
