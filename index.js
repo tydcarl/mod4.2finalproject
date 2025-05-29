651ceb3604b429c7fd70544bfcf2d5aad325ae55
Tyler <tydcarl@gmail.com>
2025-05-29 16:51:41 -0700

    map

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ async function main() {
 const users = await fetch("https://www.omdbapi.com/?apikey=485a0dbd&s=fast")
 const usersData = await users.json(); 
  console.log(usersData);
+ usersData.map(user => )
 }
 
 main(); 
