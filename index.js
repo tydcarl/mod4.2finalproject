bcf353bfb96b459ca3fc0197cc891638a75e5e31
Tyler <tydcarl@gmail.com>
2025-05-29 17:00:54 -0700

    k

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 async function main() {
 
-const users = await fetch("https://www.omdbapi.com/?apikey=485a0dbd&s=fast")
+const users = await fetch("https://www.omdbapi.com/?apikey=485a0dbd&s=fast");
 const usersData = await users.json(); 
- console.log(usersData);
- usersData.map(user => )
+ console.log(
+   usersData.map(
+     (user) => `    <div class="container">
+        <h2>Search Results</h2>
+        <div id="movie-results" class="movie-grid"></div>
+      </div>`
+   )
+ );
+ 
 }
 
 main(); 
