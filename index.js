ec64d0fbf39c03f4ff427b499f304abf1611bf51
Tyler <tydcarl@gmail.com>
2025-05-29 18:46:40 -0700

    what

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
-async function main() {
+const movieResults = document.getElementById("movie-results");
 
-const users = await fetch("https://www.omdbapi.com/?apikey=485a0dbd&s=fast");
-const usersData = await users.json(); 
- console.log(
-   usersData.map(
-     (user) => `    <div class="container">
-        <h2>Search Results</h2>
-        <div id="movie-results" class="movie-grid"></div>
-      </div>`
-   )
- );
- 
+async function main() {
+  const users = await fetch("https://www.omdbapi.com/?apikey=485a0dbd&s=fast");
+  const usersData = await users.json();
+  console.log(usersData.Search);
+
+  movieResults.innerHTML = usersData.Search.map((movie) => {
+    return `
+        <div class="movie-card">
+          <div class="movie-poster">
+            <img src="${movie.Poster}" alt="${movie.Title} Poster" onerror="this.onerror=null;this.src='https://placehold.co/200x300/cccccc/333333?text=No+Image';">
+          </div>
+          <div class="movie-info">
+            <h3>${movie.Title}</h3>
+            <p>${movie.Year}</p>
+          </div>
+        </div>
+        `;
+  });
 }
 
-main(); 
+main();
