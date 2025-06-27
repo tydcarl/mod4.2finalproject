96b0bcf9c44784552b4c657f3e95e135f6e6ff2b
Tyler <tydcarl@gmail.com>
2025-06-27 09:29:53 -0700

    loading

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 //GRABBING ELEMENTS FROM HTML
 
-const moviesWrapper = document.querySelector('.movie-grid')
-const searchName = document.querySelector('.searchName')
+const moviesWrapper = document.querySelector(".movie-grid");
+const searchName = document.querySelector(".searchName");
 
-
-
-
-//GLOBAL MOVIES VARIABLE 
+//GLOBAL MOVIES VARIABLE
 
 let currentMovies = [];
 
 //HANDLING THE SEARCH
-
 
 function searchChange(event) {
   renderMovies(event.target.value);
@@ -19,15 +15,19 @@ function searchChange(event) {
 }
 // REDNERING MOVIES CALLING API
 
-async function renderMovies (searchTerm) {
-  const response = await fetch(`https://www.omdbapi.com/?apikey=485a0dbd&s=${searchTerm}`)
-  const data = await response.json()
-  currentMovies = data.Search
-  displayMovies(currentMovies)
- 
+async function renderMovies(searchTerm) {
+  const response = await fetch(
+    `https://www.omdbapi.com/?apikey=485a0dbd&s=${searchTerm}`
+  );
+  moviesWrapper.classList += ' movies__loading'
+  const data = await response.json();
+  moviesWrapper.classList.remove(" movies__loading");
+
+  currentMovies = data.Search;
+  displayMovies(currentMovies);
 }
 
-// DISPLAYING MOVIES 
+// DISPLAYING MOVIES
 function displayMovies(movieList) {
   moviesWrapper.innerHTML = movieList
     .slice(0, 6)
@@ -44,18 +44,18 @@ function displayMovies(movieList) {
     .join("");
 }
 
-//SORTING MOVIES 
+//SORTING MOVIES
 
 function sortChange(event) {
- const sortOption = event.target.value
+  const sortOption = event.target.value;
 
- let sortedMovies =  [...currentMovies]
+  let sortedMovies = [...currentMovies];
 
- if (sortOption === "newest") {
-sortedMovies.sort((a, b) => b.Year - a.Year)
- } else if (sortOption === "oldest" ){
-  sortedMovies.sort((a, b) => a.Year - b.Year);
- }
+  if (sortOption === "newest") {
+    sortedMovies.sort((a, b) => b.Year - a.Year);
+  } else if (sortOption === "oldest") {
+    sortedMovies.sort((a, b) => a.Year - b.Year);
+  }
 
- displayMovies(sortedMovies);
+  displayMovies(sortedMovies);
 }
